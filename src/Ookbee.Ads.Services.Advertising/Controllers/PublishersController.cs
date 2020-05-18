57a5a38ae3c3c551415c1e7d86eeec64573e0782
Nath <nat@ookbee.com>
2020-05-18 10:12:31 +0700

    add publisher services

---
+++ src/Ookbee.Ads.Services.Advertising/Controllers/PublishersController.cs
@@ -0,0 +1,40 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+using Ookbee.Ads.Application.Business.Publisher;
+using Ookbee.Ads.Application.Business.Publisher.Commands.CreatePublisher;
+using Ookbee.Ads.Application.Business.Publisher.Commands.DeletePublisher;
+using Ookbee.Ads.Application.Business.Publisher.Commands.UpdatePublisher;
+using Ookbee.Ads.Application.Business.Publisher.Queries.GetPublisherById;
+using Ookbee.Ads.Application.Business.Publisher.Queries.GetPublisherList;
+using Ookbee.Ads.Common.AspNetCore.Controllers;
+using Ookbee.Ads.Common.Result;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Ookbee.Ads.Services.Advertising.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PublishersController : ApiController
+    {
+        [HttpGet]
+        public async Task<HttpResult<IEnumerable<PublisherDto>>> GetList([FromQuery]int start, [FromQuery] int length)
+            => await Mediator.Send(new GetPublisherListQuery(start, length));
+
+        [HttpGet("{id}")]
+        public async Task<HttpResult<PublisherDto>> GetById([FromRoute]string id)
+            => await Mediator.Send(new GetPublisherByIdQuery(id));
+
+        [HttpPost]
+        public async Task<HttpResult<string>> Create([FromBody]CreatePublisherCommand request)
+            => await Mediator.Send(new CreatePublisherCommand(request));
+
+        [HttpPut("{id}")]
+        public async Task<HttpResult<bool>> Update([FromRoute]string id, [FromBody]UpdatePublisherCommand request)
+            => await Mediator.Send(new UpdatePublisherCommand(id, request));
+
+        [HttpDelete("{id}")]
+        public async Task<HttpResult<bool>> Delete([FromRoute]string id)
+            => await Mediator.Send(new DeletePublisherCommand(id));
+    }
+}
