af8ba87669f7dee29a64cba95238543eb7c12745
nat <nat@ookbee.com>
2020-06-16 13:58:50 +0700

    format code

--- src/Ookbee.Ads.Services.Banner/Program.cs
+++ src/Ookbee.Ads.Services.Banner/Program.cs
@@ -1,6 +1,5 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
-using System.IO;
 
 namespace Ookbee.Ads.Services.Banner
 {
@@ -10,7 +9,6 @@ namespace Ookbee.Ads.Services.Banner
         {
             CreateHostBuilder(args).Build().Run();
         }
-
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
