8fd7eaa88d579f82c0fd95aee53a1223a8a00066
Tarntanate M <tarntanate@ookbee.com>
2020-06-16 14:56:21 +0700

    temp disable validation rules for AdList

--- src/Ookbee.Ads.Application/Business/Ad/Queries/GetAdList/GetAdListQueryValidator.cs
+++ src/Ookbee.Ads.Application/Business/Ad/Queries/GetAdList/GetAdListQueryValidator.cs
@@ -6,8 +6,8 @@ namespace Ookbee.Ads.Application.Business.Ad.Queries.GetAdList
     {
         public GetAdListQueryValidator()
         {
-            RuleFor(p => p.AdUnitId).GreaterThan(0).LessThanOrEqualTo(long.MaxValue).When(m => m.AdUnitId != null);
-            RuleFor(p => p.CampaignId).GreaterThan(0).LessThanOrEqualTo(long.MaxValue).When(m => m.CampaignId != null);
+            // RuleFor(p => p.AdUnitId).GreaterThan(0).LessThanOrEqualTo(long.MaxValue).When(m => m.AdUnitId != null);
+            // RuleFor(p => p.CampaignId).GreaterThan(0).LessThanOrEqualTo(long.MaxValue).When(m => m.CampaignId != null);
             RuleFor(p => p.Start).GreaterThanOrEqualTo(0);
             RuleFor(p => p.Length).GreaterThan(0).LessThanOrEqualTo(100);
         }
