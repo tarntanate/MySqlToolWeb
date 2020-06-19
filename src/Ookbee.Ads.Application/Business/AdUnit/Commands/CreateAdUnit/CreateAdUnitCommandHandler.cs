550c121eb2b95f804ad691f06313b2aa82d3208a
nat <nat@ookbee.com>
2020-06-19 12:31:48 +0700

    fix create adunit not validate relate value

--- src/Ookbee.Ads.Application/Business/AdUnit/Commands/CreateAdUnit/CreateAdUnitCommandHandler.cs
+++ src/Ookbee.Ads.Application/Business/AdUnit/Commands/CreateAdUnit/CreateAdUnitCommandHandler.cs
@@ -1,11 +1,13 @@
-﻿using AgileObjects.AgileMapper;
+﻿using System.Threading;
+using System.Threading.Tasks;
+using AgileObjects.AgileMapper;
 using MediatR;
 using Ookbee.Ads.Application.Business.AdUnit.Queries.IsExistsAdUnitByName;
+using Ookbee.Ads.Application.Business.AdUnitType.Queries.IsExistsAdUnitTypeById;
+using Ookbee.Ads.Application.Business.Publisher.Queries.IsExistsPublisherById;
 using Ookbee.Ads.Common.Result;
 using Ookbee.Ads.Domain.Entities.AdsEntities;
 using Ookbee.Ads.Persistence.EFCore.AdsDb;
-using System.Threading;
-using System.Threading.Tasks;
 
 namespace Ookbee.Ads.Application.Business.AdUnit.Commands.CreateAdUnit
 {
@@ -32,15 +34,23 @@ namespace Ookbee.Ads.Application.Business.AdUnit.Commands.CreateAdUnit
         {
             var result = new HttpResult<long>();
 
+            var isExistsAdUnitTypeById = await Mediator.Send(new IsExistsAdUnitTypeByIdQuery(request.AdUnitTypeId));
+            if (!isExistsAdUnitTypeById.Ok)
+                return result.Fail(isExistsAdUnitTypeById.StatusCode, isExistsAdUnitTypeById.Message);
+
+            var isExistsPublisherById = await Mediator.Send(new IsExistsPublisherByIdQuery(request.PublisherId));
+            if (!isExistsPublisherById.Ok)
+                return result.Fail(isExistsPublisherById.StatusCode, isExistsPublisherById.Message);
+
             var isExistsAdUnitByName = await Mediator.Send(new IsExistsAdUnitByNameQuery(request.Name));
             if (isExistsAdUnitByName.Ok)
                 return result.Fail(409, $"AdUnit '{request.Name}' already exists.");
 
             var entity = Mapper
                 .Map(request)
-                .ToANew<AdUnitEntity>(cfg => 
-                    cfg.Ignore(m => m.AdUnitType, m => m.Publisher));
-                    
+                .ToANew<AdUnitEntity>(cfg =>
+                   cfg.Ignore(m => m.AdUnitType, m => m.Publisher));
+
             await AdUnitDbRepo.InsertAsync(entity);
             await AdUnitDbRepo.SaveChangesAsync();
             return result.Success(entity.Id);
