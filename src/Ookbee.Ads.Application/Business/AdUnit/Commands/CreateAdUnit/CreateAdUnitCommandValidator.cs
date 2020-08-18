93e53e926fd12f7e47cb1b1bfcdffbcd0236f7e3
Tarntanate M <tarntanate@ookbee.com>
2020-08-18 11:43:54 +0700

    Fix AdUnit validation

--- src/Ookbee.Ads.Application/Business/AdUnit/Commands/CreateAdUnit/CreateAdUnitCommandValidator.cs
+++ src/Ookbee.Ads.Application/Business/AdUnit/Commands/CreateAdUnit/CreateAdUnitCommandValidator.cs
@@ -13,21 +13,27 @@ namespace Ookbee.Ads.Application.Business.AdUnit.Commands.CreateAdUnit
             Mediator = mediator;
             CascadeMode = CascadeMode.StopOnFirstFailure;
 
+            RuleFor(p => p.AdGroupId)
+                .NotNull();
+
+            RuleFor(p => p.AdNetwork)
+                .NotNull()
+                .NotEmpty()
+                .MaximumLength(10);
+
+             RuleFor(p => p.AdNetworkUnitId)
+                .NotNull()
+                .NotEmpty()
+                .MaximumLength(50);
+
+            // In one Ad Group cannot have duplicate AdNetwork
             RuleFor(p => new { AdNetwork = p.AdNetwork, AdGroupId = p.AdGroupId })
-                // .NotNull()
-                // .NotEmpty()
-                // .MaximumLength(10)
                 .CustomAsync(async (value, context, CancellationToken) =>
                 {
                     var IsExistsAdNetworkNameByAdGroup = await Mediator.Send(new IsExistsAdNetworkNameByAdGroupQuery(adNetworkName: value.AdNetwork, adGroupId: value.AdGroupId), CancellationToken);
                     if (IsExistsAdNetworkNameByAdGroup.Ok)
                         context.AddFailure($"'{value.AdNetwork}' already exists in groupId {value.AdGroupId}.");
-                });
-
-            RuleFor(p => p.AdNetworkUnitId)
-                .NotNull()
-                .NotEmpty()
-                .MaximumLength(50);
+                });          
         }
     }
 }
