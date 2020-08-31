a85738f50846c615cafe91115ba10595bf6c5694
Tarntanate M <tarntanate@ookbee.com>
2020-08-31 17:46:47 +0700

    Update AdUnit Cache with analytics

--- src/Ookbee.Ads.Application/Business/Cache/Commands/AdUnitCache/AdUnitCacheDto.cs
+++ src/Ookbee.Ads.Application/Business/Cache/Commands/AdUnitCache/AdUnitCacheDto.cs
@@ -1,9 +1,13 @@
-﻿namespace Ookbee.Ads.Application.Business.Cache.AdUnitCache
+﻿using Ookbee.Ads.Application.Business.Cache.AdAssetCache;
+
+namespace Ookbee.Ads.Application.Business.Cache.AdUnitCache
 {
     public class AdUnitCacheDto
     {
         public string Id { get; set; }
         public string Name { get; set; }
         public string UnitId { get; set; }
+
+        public AdAnalyticsCacheDto Analytics { get; set; }
     }
 }
--- src/Ookbee.Ads.Application/Business/Cache/Commands/AdUnitCache/AdUnitCacheMappingProfile.cs
+++ src/Ookbee.Ads.Application/Business/Cache/Commands/AdUnitCache/AdUnitCacheMappingProfile.cs
@@ -1,5 +1,8 @@
-﻿using AutoMapper;
+﻿using System.Collections.Generic;
+using AutoMapper;
 using Ookbee.Ads.Application.Business.AdUnit;
+using Ookbee.Ads.Application.Business.Cache.AdAssetCache;
+using Ookbee.Ads.Infrastructure;
 
 namespace Ookbee.Ads.Application.Business.Cache.AdUnitCache
 {
@@ -9,7 +12,23 @@ namespace Ookbee.Ads.Application.Business.Cache.AdUnitCache
         {
             CreateMap<AdUnitDto, AdUnitCacheDto>()
                 .ForMember(dest => dest.Id, m => m.MapFrom(src => src.AdNetwork.ToLower() == "ookbee" ? src.Id.ToString() : src.AdNetworkUnitId))
-                .ForMember(dest => dest.Name, m => m.MapFrom(src => src.AdNetwork));
+                .ForMember(dest => dest.Name, m => m.MapFrom(src => src.AdNetwork))
+                .ForMember(dest => dest.Analytics, m => m.MapFrom(src => src.AdNetwork.ToLower() == "ookbee" ? null : AnalyticsConverter(src.Id)));
+        }
+
+        private AdAnalyticsCacheDto AnalyticsConverter(long adUnitId)
+        {
+            var analyticsBaseUrl = GlobalVar.AppSettings.Services.Ads.Analytics.BaseUri.External;
+
+            var impressions = new List<string>() { $"{analyticsBaseUrl}/api/units/{adUnitId}/stats?event=impression" };
+            var clicks = new List<string>() { $"{analyticsBaseUrl}/api/units/{adUnitId}/stats?event=click" };
+        
+            var analyticsCache = new AdAnalyticsCacheDto()
+            {
+                Clicks = clicks,
+                Impressions = impressions,
+            };
+            return analyticsCache;
         }
     }
 }
