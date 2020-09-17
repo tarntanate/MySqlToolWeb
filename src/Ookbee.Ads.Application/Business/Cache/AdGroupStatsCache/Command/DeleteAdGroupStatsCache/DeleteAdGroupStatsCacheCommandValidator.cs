aec9ef5b45236d86bfad279aba9fbc9b6549429b
NathZ <nathz@NathZs-MacBook-Pro.local>
2020-09-17 10:49:15 +0700

    format code

---
+++ src/Ookbee.Ads.Application/Business/Cache/AdGroupStatsCache/Command/DeleteAdGroupStatsCache/DeleteAdGroupStatsCacheCommandValidator.cs
@@ -0,0 +1,12 @@
+﻿using FluentValidation;
+
+namespace Ookbee.Ads.Application.Business.Cache.AdGroupStatsCache.Commands.DeleteAdGroupStatsCache
+{
+    public class DeleteAdGroupStatsCacheCommandValidator : AbstractValidator<DeleteAdGroupStatsCacheCommand>
+    {
+        public DeleteAdGroupStatsCacheCommandValidator()
+        {
+
+        }
+    }
+}
