34f135fd458f811a760aa48f12d8dba34dafbf7f
NathZ <nathz@NathZs-MacBook-Pro.local>
2020-09-21 15:44:29 +0700

    fix bot not run at all

--- src/Ookbee.Ads.Application/Infrastructure/ArchiveAnalyticsService.cs
+++ src/Ookbee.Ads.Application/Infrastructure/ArchiveAnalyticsService.cs
@@ -1,7 +1,6 @@
 using MediatR;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Ookbee.Ads.Application.Business.Cache.AdGroupStats.Commands.ArchiveAdGroupStats;
 using Ookbee.Ads.Common;
 using Ookbee.Ads.Common.Extensions;
@@ -31,22 +30,12 @@ namespace Ookbee.Ads.Application.Infrastructure
                     var next = true;
                     do
                     {
-                        try
-                        {
-                            var caculatedAt = MechineDateTime.Date;
-                            Console.WriteLine(caculatedAt);
-                            await mediator.Send(new ArchiveAdGroupStatsCommand(caculatedAt), cancellationToken);
-                            var nowDateTime = MechineDateTime.Now;
-                            var nextDateTime = nowDateTime.RoundUp(TimeSpan.FromSeconds(3));
-                            var timeout = nextDateTime - nowDateTime;
-                            Console.WriteLine(timeout);
-                            Thread.Sleep(timeout);
-                        }
-                        catch (Exception ex)
-                        {
-                            Console.WriteLine(ex);
-                            Thread.Sleep(50000);
-                        }
+                        var caculatedAt = MechineDateTime.Date;
+                        await mediator.Send(new ArchiveAdGroupStatsCommand(caculatedAt), cancellationToken);
+                        var nowDateTime = MechineDateTime.Now;
+                        var nextDateTime = nowDateTime.RoundUp(TimeSpan.FromSeconds(3));
+                        var timeout = nextDateTime - nowDateTime;
+                        Thread.Sleep(timeout);
                     }
                     while (next);
                 }
