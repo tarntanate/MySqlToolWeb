3038692624d390d033711f9a0ece40376710ed42
Tarntanate M <tarntanate@ookbee.com>
2020-09-28 10:56:28 +0700

    Fix Bug User Role Mappings

--- src/Ookbee.Ads.Application/Services/Identity/UserRoleMapping/Queries/GetUserRoleMappingList/GetUserRoleMappingListQueryHandler.cs
+++ src/Ookbee.Ads.Application/Services/Identity/UserRoleMapping/Queries/GetUserRoleMappingList/GetUserRoleMappingListQueryHandler.cs
@@ -40,9 +40,7 @@ namespace Ookbee.Ads.Application.Services.Identity.UserRoleMapping.Queries.GetUs
             );
 
             var result = new Response<IEnumerable<UserRoleMappingDto>>();
-            return (items.HasValue())
-                ? result.Success(items)
-                : result.Fail(404, $"Data not found.");
+            return result.Success(items);
         }
     }
 }
