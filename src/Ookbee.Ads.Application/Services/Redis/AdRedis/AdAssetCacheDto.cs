9b128800fcb2e7e99a2b1e96cbad9a8edad52aca
NathZ <nathz@NathZs-MacBook-Pro.local>
2020-10-21 10:33:39 +0700

    wrap json on publish api

---
+++ src/Ookbee.Ads.Application/Services/Redis/AdRedis/AdAssetCacheDto.cs
@@ -0,0 +1,11 @@
+using Ookbee.Ads.Infrastructure.Models;
+
+namespace Ookbee.Ads.Application.Services.Redis.AdRedis
+{
+    public class AdAssetCacheDto
+    {
+        public string AssetType {get; set;}
+        public string AssetUrl {get; set;}
+        public AdPosition Position {get; set;}
+    }
+}
--- src/Ookbee.Ads.Application/Services/Redis/AnalyticsCacheDto.cs
+++ src/Ookbee.Ads.Application/Services/Redis/AnalyticsCacheDto.cs
@@ -1,8 +1,8 @@
 using System.Collections.Generic;
 
-namespace Ookbee.Ads.Application.Services.Redis.AdUnitRedis
+namespace Ookbee.Ads.Application.Services.Redis
 {
-    public class AdUnitStatsCacheDto
+    public class AnalyticsCacheDto
     {
         public IEnumerable<string> Clicks { get; set; }
         public IEnumerable<string> Impressions { get; set; }
