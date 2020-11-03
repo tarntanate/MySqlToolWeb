8bbe49c96711146bfb3a176943e0d7869fd763c2
Tarntanate M <tarntanate@ookbee.com>
2020-11-03 11:37:18 +0700

    Refactor GetAdByUnitId

--- src/Ookbee.Ads.Services.Publish/Controllers/UnitsController.cs
+++ src/Ookbee.Ads.Services.Publish/Controllers/UnitsController.cs
@@ -12,7 +12,13 @@ namespace Ookbee.Ads.Services.Publish.Controllers
     public class UnitsController : ApiController
     {
         [HttpGet("{unitId}/ad")]
-        public async Task<ContentResult> GetAdByUnitId([FromQuery] string platform, [FromRoute] long unitId, [FromQuery] string ookbeeId, [FromHeader(Name = "Ookbee-Account-Id")] string ookbeeId_header, [FromHeader(Name = "Ookbee-Device-Id")] string deviceId, CancellationToken cancellationToken)
+        public async Task<ContentResult> GetAdByUnitId(
+            [FromQuery] string platform, 
+            [FromRoute] long unitId, 
+            [FromQuery] string ookbeeId, 
+            [FromHeader(Name = "Ookbee-Account-Id")] string ookbeeId_header, 
+            [FromHeader(Name = "Ookbee-Device-Id")] string deviceId, 
+            CancellationToken cancellationToken)
         {
             long? userId = Convert.ToInt32(ookbeeId_header ?? ookbeeId ?? deviceId);
             var result = await Mediator.Send(new GetAdRedisQuery(platform, unitId, userId: userId), cancellationToken);
