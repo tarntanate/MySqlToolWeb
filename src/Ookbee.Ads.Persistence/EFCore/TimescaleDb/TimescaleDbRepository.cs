93469b0027dfa8d76ec9680b99799a4e414687f4
Tarntanate M <tarntanate@ookbee.com>
2020-11-11 16:36:36 +0700

    Fix merged AutoMapper

--- src/Ookbee.Ads.Persistence/EFCore/TimescaleDb/TimescaleDbRepository.cs
+++ src/Ookbee.Ads.Persistence/EFCore/TimescaleDb/TimescaleDbRepository.cs
@@ -1,11 +1,12 @@
-﻿using Ookbee.Ads.Common.EntityFrameworkCore.Domain;
+﻿using AutoMapper;
+using Ookbee.Ads.Common.EntityFrameworkCore.Domain;
 using Ookbee.Ads.Common.EntityFrameworkCore.Repository;
 
 namespace Ookbee.Ads.Persistence.EFCore.TimescaleDb
 {
     public class TimescaleDbRepository<TEntity> : BaseRepository<TEntity> where TEntity : BaseEntity
     {
-        public TimescaleDbRepository(TimescaleDbContext context) : base(context)
+        public TimescaleDbRepository(IMapper mapper, TimescaleDbContext context) : base(mapper, context)
         {
 
         }
